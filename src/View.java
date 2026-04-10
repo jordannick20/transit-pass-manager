fd3dc5956697b71ed9d6e27f2724e1fe60a9dcc8
jordan <nicholsonjordan48@gmail.com>
2026-04-10 13:01:06 -0400

    view done

--- src/View.java
+++ src/View.java
@@ -2,25 +2,58 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
 import java.awt.Font;
 import net.miginfocom.swing.MigLayout;
 import java.awt.CardLayout;
 import java.awt.Dimension;
+import java.util.ArrayList;
+import java.awt.event.*;
 
 public class View extends JFrame {
+
+    // Layout and Main Containers
     private CardLayout cardLayout;
     private JPanel mainContainer;
-    private JPanel creatPanel;
+
+    // Screen Panels
     private JPanel viewPanel;
     private JPanel scanPanel;
+    private JPanel topupPanel;
+    private JPanel deletePanel;
+    private JPanel selectPanel;
 
+    // Top-Up Screen stuff
+    private JButton btnTopUpOk;
+    private JButton btnTopUpCancel;
+    private JTextField txtTopUpAmount;
+
+    // Delete Screen stuff
+    private JLabel lblDeletePassName;
+    private JButton btnDeleteOk;
+    private JButton btnDeleteCancel;
+
+    // Select Screen stuff
+    private JList<String> lstPasses;
+    private DefaultListModel<String> listModel;
+    private JButton btnSelectOk;
+    private JButton btnSelectCancel;
+
+    // Scan Screen stuff
+    private JLabel lblScanPassName;
+    private JButton btnScanOk;
+    private JButton btnScanCancel;
+
+    // View Screen stuff
     private JTextArea txtViewOutput;
 
+    // Main Menu Buttons
     private JButton btnSelect;
     private JButton btnTopup;
     private JButton btnScan;
@@ -28,17 +61,18 @@ public class View extends JFrame {
     private JButton btnDelete;
     private JButton btnView;
     private JButton btnQuit;
+
+    // Other Buttons 
     private JButton btnViewBack;
-    private JButton btnOk;
-    
-    private JComboBox<String> cmbPassType;
-    private JTextField txtHolderName;
-    private JTextField txtInitialBalance;
     private JButton btnCreatePass;
     private JButton btnBack;
 
+    private JComboBox<String> cmbPassType;
+    private JTextField txtHolderName;
+    private JTextField txtInitialBalance;
+    
 
-
+    // Constructor
     public View() {
         setTitle("Transit Pass");
         setSize(700, 500);
@@ -53,13 +87,19 @@ public class View extends JFrame {
         buildCreatePanel();
         buildViewPanel();
         buildScanPanel();
+        buildTopupPanel();
+        buildSelectPanel();
+        buildDeletePanel();
+        
     }
-    
+
+    // using cardlayout to build Panels 
+
     private void buildMainMenuPanel() {
         JPanel mainMenuPanel = new JPanel(new MigLayout("insets 45", "[]25[]", "[]25[][][][][][][]"));
 
         JLabel lblTitle = new JLabel("Transit Pass Management System v1.0 >");
-        lblTitle.setFont(new Font(Font.MONOSPACED,Font.BOLD, 24));
+        lblTitle.setFont(new Font(Font.MONOSPACED, Font.BOLD, 24));
         mainMenuPanel.add(lblTitle, "span 2, wrap");
 
         btnSelect = new JButton("select");
@@ -89,29 +129,16 @@ public class View extends JFrame {
         btnQuit = new JButton("quit");
         mainMenuPanel.add(btnQuit);
         mainMenuPanel.add(new JLabel("Save and Quit"), "wrap");
+        setMainButtonsEnabled(false);
 
         mainContainer.add(mainMenuPanel, "MAIN");
-
-        btnQuit.addActionListener(e -> {
-            System.exit(0);
-        });
-
-        btnCreate.addActionListener(e -> {
-            cardLayout.show(mainContainer, "CREATE");
-        });
-        btnView.addActionListener(e -> {
-            cardLayout.show(mainContainer, "VIEW");
-        });
-        btnScan.addActionListener(e -> {
-            cardLayout.show(mainContainer, "SCAN");
-        });
     }
-    
+
     private void buildCreatePanel() {
         JPanel createPanel = new JPanel(new MigLayout("insets 25", "[][grow]", "[]20[]15[]15[]25[]"));
 
         JLabel lblCreateTitle = new JLabel("Transit Pass Management System v1.0 >");
-        lblCreateTitle.setFont(new Font(Font.MONOSPACED,Font.BOLD, 24));
+        lblCreateTitle.setFont(new Font(Font.MONOSPACED, Font.BOLD, 24));
         createPanel.add(lblCreateTitle, "span 2, wrap");
 
         createPanel.add(new JLabel("Pass Type:"));
@@ -133,12 +160,6 @@ public class View extends JFrame {
         createPanel.add(btnCreatePass);
         createPanel.add(btnBack);
 
-        // btnCreatePass.addActionListener(e -> onCreatePass());
-        btnBack.addActionListener(e -> {
-            clearCreateFields();
-            cardLayout.show(mainContainer, "MAIN");
-        });
-
         mainContainer.add(createPanel, "CREATE");
     }
 
@@ -146,7 +167,7 @@ public class View extends JFrame {
         viewPanel = new JPanel(new MigLayout("insets 25", "[grow]", "[]20[grow]20[]"));
 
         JLabel lblTitle = new JLabel("Transit Pass Management System v1.0 >");
-        lblTitle.setFont(new Font(Font.MONOSPACED,Font.BOLD, 24));
+        lblTitle.setFont(new Font(Font.MONOSPACED, Font.BOLD, 24));
         viewPanel.add(lblTitle, "wrap");
 
         txtViewOutput = new JTextArea(15, 40);
@@ -158,31 +179,229 @@ public class View extends JFrame {
         btnViewBack = new JButton("Back");
         viewPanel.add(btnViewBack, "right");
 
-        btnViewBack.addActionListener(e -> {
-            cardLayout.show(mainContainer, "MAIN");
-        });
-
         mainContainer.add(viewPanel, "VIEW");
-}
-    private void buildScanPanel() {
-        scanPanel = new JPanel(new MigLayout("insets 25", "[]", "[]20[]20[]"));
-
-        JLabel lblTitle = new JLabel("Transit Pass Management System v1.0 >");
-        lblTitle.setFont(new Font(Font.MONOSPACED,Font.BOLD, 24));
-        scanPanel.add(lblTitle, "wrap");
-
-        mainContainer.add(scanPanel, "SCAN");
-
     }
 
-    private void clearCreateFields() {
-        txtHolderName.setText("");
-        txtInitialBalance.setText("");
-        cmbPassType.setSelectedIndex(0);
-    }  
+    private void buildScanPanel() {
+        scanPanel = new JPanel(new MigLayout("insets 35", "[]20[]", "[]30[]30[]"));
 
+        JLabel lblTitle = new JLabel("Transit Pass Management System v1.0 >");
+        lblTitle.setFont(new Font(Font.MONOSPACED, Font.BOLD, 24));
+        scanPanel.add(lblTitle, "span 2, wrap");
+
+        JLabel lblQuestion = new JLabel("Scan Current Pass?");
+        lblQuestion.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 18));
+        scanPanel.add(lblQuestion);
+
+        lblScanPassName = new JLabel("");
+        lblScanPassName.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 18));
+        scanPanel.add(lblScanPassName, "wrap");
+
+        btnScanOk = new JButton("Ok");
+        btnScanCancel = new JButton("Cancel");
+
+        scanPanel.add(btnScanOk, "split 2, gapleft 130");
+        scanPanel.add(btnScanCancel);
+
+        mainContainer.add(scanPanel, "SCAN");
+    }
+
+    private void buildTopupPanel() {
+        topupPanel = new JPanel(new MigLayout("insets 40", "[]10[]", "[]40[]30[]"));
+
+        JLabel lblTitle = new JLabel("Transit Pass Management System v1.0 >");
+        lblTitle.setFont(new Font(Font.MONOSPACED, Font.BOLD, 24));
+        topupPanel.add(lblTitle, "span 2, wrap");
+
+        JLabel lblPrompt = new JLabel("Enter Amount to top-up: $");
+        lblPrompt.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 16));
+        topupPanel.add(lblPrompt);
+
+        txtTopUpAmount = new JTextField(10);
+        topupPanel.add(txtTopUpAmount, "wrap");
+
+        btnTopUpOk = new JButton("Ok");
+        btnTopUpCancel = new JButton("Cancel");
+
+        topupPanel.add(btnTopUpOk, "split 2, gapleft 150");
+        topupPanel.add(btnTopUpCancel);
+
+        mainContainer.add(topupPanel, "TOPUP");
+    }
+
+    private void buildSelectPanel() {
+        selectPanel = new JPanel(new MigLayout("insets 35", "[]20[grow]", "[]30[grow]30[]"));
+
+        JLabel lblTitle = new JLabel("Transit Pass Management System v1.0 >");
+        lblTitle.setFont(new Font(Font.MONOSPACED, Font.BOLD, 24));
+        selectPanel.add(lblTitle, "span 2, wrap");
+
+        JLabel lblSelect = new JLabel("Select Pass:");
+        lblSelect.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 18));
+        selectPanel.add(lblSelect, "top");
+
+        listModel = new DefaultListModel<>();
+        lstPasses = new JList<>(listModel);
+
+        JScrollPane scrollPane = new JScrollPane(lstPasses);
+        scrollPane.setPreferredSize(new Dimension(380, 270));
+        selectPanel.add(scrollPane, "grow, wrap");
+
+        btnSelectOk = new JButton("Ok");
+        btnSelectCancel = new JButton("Cancel");
+
+        selectPanel.add(btnSelectOk, "split 2, gapleft 10");
+        selectPanel.add(btnSelectCancel);
+
+        mainContainer.add(selectPanel, "SELECT");
+    }
+
+    private void buildDeletePanel() {
+        deletePanel = new JPanel(new MigLayout("insets 35", "[]20[]", "[]35[]35[]"));
+
+        JLabel lblTitle = new JLabel("Transit Pass Management System v1.0 >");
+        lblTitle.setFont(new Font(Font.MONOSPACED, Font.BOLD, 24));
+        deletePanel.add(lblTitle, "span 2, wrap");
+
+        JLabel lblQuestion = new JLabel("Delete Current Pass?");
+        lblQuestion.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 18));
+        deletePanel.add(lblQuestion);
+
+        lblDeletePassName = new JLabel("");
+        lblDeletePassName.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 18));
+        deletePanel.add(lblDeletePassName, "wrap");
+
+        btnDeleteOk = new JButton("Ok");
+        btnDeleteCancel = new JButton("Cancel");
+
+        deletePanel.add(btnDeleteOk, "split 2, gapleft 250");
+        deletePanel.add(btnDeleteCancel);
+
+        mainContainer.add(deletePanel, "DELETE");
+    }
+
+    // Screen Display Methods
+    
+
+    public void showMainMenu() {
+        cardLayout.show(mainContainer, "MAIN");
+    }
+
+    public void showTopUpScreen() {
+        txtTopUpAmount.setText("");
+        cardLayout.show(mainContainer, "TOPUP");
+    }
+
+    public void showCreateScreen() {
+        cardLayout.show(mainContainer, "CREATE");
+    }
+
+    public void showViewScreen(String text) {
+        txtViewOutput.setText(text);
+        cardLayout.show(mainContainer, "VIEW");
+    }
+
+    public void showScanScreen(String passName) {
+        lblScanPassName.setText(passName);
+        cardLayout.show(mainContainer, "SCAN");
+    }
+
+    public void showDeleteScreen(String passName) {
+        lblDeletePassName.setText(passName);
+        cardLayout.show(mainContainer, "DELETE");
+    }
+
+    public void showSelectScreen() {
+        cardLayout.show(mainContainer, "SELECT");
+    }
+
+    // Dialog Methods
+    public void showErrorMessage(String message) {
+        JOptionPane.showMessageDialog(this, message, "Input Error", JOptionPane.ERROR_MESSAGE);
+    }
+
+    public void showInfoMessage(String message) {
+        JOptionPane.showMessageDialog(this, message, "Message", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    // Button listeners
+
+    public void addQuitListener(ActionListener listener) {
+        btnQuit.addActionListener(listener);
+    }
+
+    public void addCreateListener(ActionListener listener) {
+        btnCreate.addActionListener(listener);
+    }
+
+    public void addCreatePassListener(ActionListener listener) {
+        btnCreatePass.addActionListener(listener);
+    }
+
+    public void addCreateBackListener(ActionListener listener) {
+        btnBack.addActionListener(listener);
+    }
+
+    public void addViewListener(ActionListener listener) {
+        btnView.addActionListener(listener);
+    }
+
+    public void addViewBackListener(ActionListener listener) {
+        btnViewBack.addActionListener(listener);
+    }
+    
+    public void addScanListener(ActionListener listener) {
+        btnScan.addActionListener(listener);
+    }
+
+    public void addScanOkListener(ActionListener listener) {
+        btnScanOk.addActionListener(listener);
+    }
+
+    public void addScanCancelListener(ActionListener listener) {
+        btnScanCancel.addActionListener(listener);
+    }
+
+    public void addTopUpListener(ActionListener listener) {
+        btnTopup.addActionListener(listener);
+    }
+
+    public void addTopUpOkListener(ActionListener listener) {
+        btnTopUpOk.addActionListener(listener);
+    }
+
+    public void addTopUpCancelListener(ActionListener listener) {
+        btnTopUpCancel.addActionListener(listener);
+    }
+
+    public void addSelectListener(ActionListener listener) {
+        btnSelect.addActionListener(listener);
+    }
+
+    public void addSelectOkListener(ActionListener listener) {
+        btnSelectOk.addActionListener(listener);
+    }
+
+    public void addSelectCancelListener(ActionListener listener) {
+        btnSelectCancel.addActionListener(listener);
+    }
+
+    public void addDeleteListener(ActionListener listener) {
+        btnDelete.addActionListener(listener);
+    }
+
+    public void addDeleteOkListener(ActionListener listener) {
+        btnDeleteOk.addActionListener(listener);
+    }
+
+    public void addDeleteCancelListener(ActionListener listener) {
+        btnDeleteCancel.addActionListener(listener);
+    }
     public static void main(String[] args) {
+        Model model = new Model();
         View view = new View();
+        new Controller(model, view);
         view.setVisible(true);
+        
     }
 }
